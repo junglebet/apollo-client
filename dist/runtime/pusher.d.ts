8995995810bcd310db25969ebfb3d752a638b929
Ashot Markosyan <mkdrm.one@gmail.com>
2023-08-02 13:58:05 -0700

    chore(pusher): build

---
+++ dist/runtime/pusher.d.ts
@@ -0,0 +1,10 @@
+export default PusherLink;
+declare class PusherLink extends ApolloLink {
+    constructor(options: any);
+    pusher: any;
+    request(operation: any, forward: any): Observable<any>;
+    subscribeToChannel(subscriptionChannel: any, event: string | undefined, observer: any): void;
+    unsubscribeFromChannel(subscriptionChannel: any): void;
+}
+import { ApolloLink } from "@apollo/client/link/core";
+import { Observable } from "@apollo/client/core";
