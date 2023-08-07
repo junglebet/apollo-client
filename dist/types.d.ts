3d87e23019b6d3360ac73888a9ecb8106c22142f
Ashot Markosyan <mkdrm.one@gmail.com>
2023-08-07 11:07:56 -0700

    chore(pusher): build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,13 +1,10 @@
 
-import { ModuleOptions, ModuleHooks, ModuleRuntimeConfig, ModulePublicRuntimeConfig } from './module'
+import { ModuleOptions } from './module'
 
 declare module '@nuxt/schema' {
   interface NuxtConfig { ['apollo']?: Partial<ModuleOptions> }
   interface NuxtOptions { ['apollo']?: ModuleOptions }
-  interface NuxtHooks extends ModuleHooks {}
-  interface RuntimeConfig extends ModuleRuntimeConfig {}
-  interface PublicRuntimeConfig extends ModulePublicRuntimeConfig {}
 }
 
 
-export { default } from './module'
+export { ErrorResponse } from './module'
