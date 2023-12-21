841a2e42219d6c54936fcfea299433817cdd5953
digitdash <eric@gamba.com>
2023-12-21 15:13:01 -0500

    Update nuxt.config.ts

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -24,14 +24,14 @@ export default defineNuxtConfig({
       },
       users: {
         // TODO: this endpoint is project case and not shared one, should be shared one once they provide
-        httpEndpoint: 'https://aged-farm-ih4lzf6w0nlc.vapor-farm-f1.com/@',
+        httpEndpoint: 'https://cold-silence-fjaidfhrsyil.vapor-farm-d1.com/graph',
         httpLinkOptions: {
           credentials: 'include' // NOTE: this is required if cookie should be sent for different domain
         },
         // NOTE: `X-CSRF-TOKEN` is default csrfHeader
         csrfHeader: 'X-CSRF-TOKEN'
       },
-      gamba: {
+      junglebet: {
         httpEndpoint: `${process.env.GRAPHQL_BASE_URL}/@`,
         httpLinkOptions: {
           credentials: 'include'
